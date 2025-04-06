de64f1bd4bff62dd381041a13cba96043055c44f
Rodalit <ryzyevtimur@gmail.com>
2025-04-06 14:21:41 +0300

    Add files via upload

---
+++ sorting/bubble_sorting.py
@@ -0,0 +1,12 @@
+def bubble_sort(array):
+	n = len(array)
+
+	for j in range(n - 1):
+		for i in range(n - 1 - j):
+			if array[i] > array[i + 1]:
+				array[i], array[i + 1] = array[i + 1], array[i]
+
+	return array
+
+list1 = [2, 5, 1, 3, 4, 6, 9, 8, 7, 0]
+print(bubble_sort(list1))
