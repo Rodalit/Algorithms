9ccda6f445e00aaf6e44032e1490acc255b11de1
Rodalit <ryzyevtimur@gmail.com>
2025-04-06 14:25:05 +0300

    Replaced tabs with spaces

--- sorting/selection_sort.py
+++ sorting/selection_sort.py
@@ -1,18 +1,18 @@
 def find_smallest(array):
-	smallest = array[0]
-	smallest_index = 0
-	for i in range(1, len(array)):
-		if array[i] < smallest:
-			smallest = array[i]
-			smallest_index = i
-	return smallest_index
+    smallest = array[0]
+    smallest_index = 0
+    for i in range(1, len(array)):
+        if array[i] < smallest:
+            smallest = array[i]
+            smallest_index = i
+    return smallest_index
 
 def selection_sort(array):
-	new_array = []
-	for i in range(len(array)):
-		smallest = find_smallest(array)
-		new_array.append(array.pop(smallest))
-	return new_array
+    new_array = []
+    for i in range(len(array)):
+        smallest = find_smallest(array)
+        new_array.append(array.pop(smallest))
+    return new_array
 
 list1 = [3, 2, 8, 0, 1, 5, 9, 7, 6]
 list2 = [12, 54, 26, 89, 90, 67, 78, 49, 37]
