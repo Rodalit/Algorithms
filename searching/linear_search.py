1cfc34ffbb9ce224a99ae13cadb9625dda926eff
Rodalit <ryzyevtimur@gmail.com>
2025-04-06 14:25:48 +0300

    Replaced tabs with spaces

--- searching/linear_search.py
+++ searching/linear_search.py
@@ -1,8 +1,8 @@
 def linear_search(array, item):
-	for i in range(len(array)):
-		if array[i] == item:
-			return i
-	return None
+    for i in range(len(array)):
+        if array[i] == item:
+            return i
+    return None
 
 list1 = [3, 9, 8, 2, 1, 4, 6, 5, 7]
 print(linear_search(list1, 6))
