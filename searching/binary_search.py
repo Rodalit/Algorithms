02d713d5f16a7edccc9b6090715a18d8a9472ffa
Rodalit <ryzyevtimur@gmail.com>
2025-04-06 14:25:28 +0300

    Replaced tabs with spaces

--- searching/binary_search.py
+++ searching/binary_search.py
@@ -1,17 +1,17 @@
 def binary_search(array, item):
-	low = 0
-	high = len(array) - 1
+    low = 0
+    high = len(array) - 1
 
-	while low <= high:
-		mid = (low + high) // 2
-		guess = array[mid]
-		if guess == item:
-			return mid
-		elif guess > item:
-			high = mid - 1
-		else:
-			low = mid + 1
-	return None
+    while low <= high:
+        mid = (low + high) // 2
+        guess = array[mid]
+        if guess == item:
+            return mid
+        elif guess > item:
+            high = mid - 1
+        else:
+            low = mid + 1
+    return None
 
 list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 print(binary_search(list1, 5)) # => 4
